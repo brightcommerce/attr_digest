d2c48dd6479e1f07c4e7719f9df40b67adb7d6d2
Jurgen Jocubeit <jurgenjocubeit@gmail.com>
2016-02-16 15:31:45 +1000

    Added CodeCov.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 begin
   require 'simplecov'
   SimpleCov.start
+  if ENV['CI']=='true'
+    require 'codecov'
+    SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  end
 rescue LoadError
 end
 
