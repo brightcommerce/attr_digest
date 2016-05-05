532fc22c425e47ea3ea27a3e572b31106dbadc2f
Jurgen Jocubeit <jurgenjocubeit@gmail.com>
2016-05-05 18:55:43 +1000

    Updated to Argon2 v1.0.0 and Rails v5.0.0.beta4.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,9 @@
-v1.0.0 - Initial release.
+# Change Log
+
+##v1.1.0
+- Updated Argon2 dependency to v1.1.0.
+- Updated ActiveSupport and ActiveRecord to v5.0.0.beta4.
+- Updated README.md to reflect dependency changes and compatibility.
+
+##v1.0.0
+- Initial release.
--- lib/attr_digest/version.rb
+++ lib/attr_digest/version.rb
@@ -1,14 +1,14 @@
 module AttrDigest
   module VERSION
     MAJOR = 1
-    MINOR = 0
+    MINOR = 1
     TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
-    
+
     SUMMARY = "AttrDigest v#{STRING}"
-    
+
     DESCRIPTION = "Provides functionality to store a hash digest of an attribute using Argon2"
   end
 end
