85a8ca57b33ee6e831c282b7da6cbf2a008b0f8d
Jurgen Jocubeit <jurgenjocubeit@gmail.com>
2016-11-12 11:11:32 +1000

    Updated Version to 2.0.0

--- lib/attr_digest/version.rb
+++ lib/attr_digest/version.rb
@@ -1,7 +1,7 @@
 module AttrDigest
   module VERSION
-    MAJOR = 1
-    MINOR = 2
+    MAJOR = 2
+    MINOR = 0
     TINY  = 0
     PRE   = nil
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
